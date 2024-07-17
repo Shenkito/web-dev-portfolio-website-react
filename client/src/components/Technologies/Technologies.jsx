a8e647a270bfd3c3676601bfe8d4564ea300d766
Ivelin Shenkov <ishenkito@gmail.com>
2024-07-17 23:18:54 +0300

    Fix MongoDb icon

--- client/src/components/Technologies/Technologies.jsx
+++ client/src/components/Technologies/Technologies.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import Marquee from 'react-fast-marquee';
-import { FaReact, FaNodeJs, FaHtml5, FaCss3, FaDatabase } from 'react-icons/fa';
-import { SiJavascript, SiExpress, SiVite } from 'react-icons/si';
+import { FaReact, FaNodeJs, FaHtml5, FaCss3 } from 'react-icons/fa';
+import { SiJavascript, SiExpress, SiVite, SiMongodb } from 'react-icons/si';
 
 const Technologies = () => {
     const [marqueePaused, setMarqueePaused] = useState(false);
@@ -11,11 +11,11 @@ const Technologies = () => {
     };
 
     return (
-        <section id="about" className="text-white px-4 py-20">
+        <section id="technologies" className="text-white px-4 py-20">
             <div className="max-w-5xl mx-auto">
                 <h2 className="text-3xl md:text-4xl font-bold text-center text-white mb-10">Technologies I'm Familiar With</h2>
                 <div className="marquee-container bg-gray-900 rounded-lg p-4 xl:w-3/4 mx-auto">
-                    <Marquee gradient={false} speed={60} pauseOnHover={true} paused={marqueePaused}>
+                    <Marquee gradient={false} speed={40} pauseOnHover={true} paused={marqueePaused}>
                         <div className="icon-item mx-4" onMouseEnter={() => handleIconHover(true)} onMouseLeave={() => handleIconHover(false)}>
                             <FaReact className="text-5xl text-blue-400" />
                         </div>
@@ -38,7 +38,7 @@ const Technologies = () => {
                             <SiVite className="text-5xl text-gray-400" />
                         </div>
                         <div className="icon-item mx-4" onMouseEnter={() => handleIconHover(true)} onMouseLeave={() => handleIconHover(false)}>
-                            <FaDatabase className="text-5xl text-yellow-400" />
+                            <SiMongodb className="text-5xl text-green-400" />
                         </div>
                         {/* Add more icons as needed */}
                     </Marquee>
