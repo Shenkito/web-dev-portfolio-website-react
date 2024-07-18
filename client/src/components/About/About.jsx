43757857275d7892f6f898e1adfdf3a9f1c6f6af
Ivelin Shenkov <ishenkito@gmail.com>
2024-07-18 21:20:23 +0300

    Add color to texts

--- client/src/components/About/About.jsx
+++ client/src/components/About/About.jsx
@@ -10,7 +10,7 @@ const About = () => {
             <div className="flex flex-col md:flex-row items-center justify-between w-full max-w-5xl space-y-4 md:space-y-0 md:space-x-10">
                 <div className="text-center space-y-4 md:flex-1">
                     <p className="leading-relaxed text-sm md:text-base">
-                        My journey in the world of coding began at SoftUni in Sofia, Bulgaria, where I honed my skills in JavaScript, ReactJS, and various web technologies. With a strong foundation in front-end and back-end development, I'm driven by a desire to solve problems and continuously learn.
+                        My journey in the world of coding began at <span className="text-blue-400">SoftUni</span> in Sofia, Bulgaria, where I honed my skills in JavaScript, ReactJS, and various web technologies. With a strong foundation in front-end and back-end development, I'm driven by a desire to solve problems and continuously learn.
                     </p>
                     <p className="leading-relaxed text-sm md:text-base">
                         In addition to my technical expertise, I bring excellent communication skills and a positive attitude. I'm excited to bring my diverse background and technical skills to the world of software engineering.
@@ -18,7 +18,7 @@ const About = () => {
                     <p className="leading-relaxed text-sm md:text-base">
                         When I'm not coding, you can find me enjoying sports and indulging in my love for music. My hobbies keep me balanced and fuel my creativity, allowing me to approach challenges with a fresh and dynamic perspective.
                     </p>
-                    <p className="leading-relaxed text-sm md:text-base">
+                    <p className="leading-relaxed text-sm md:text-base text-green-400 font-bold">
                         Let's connect and build something amazing together!
                     </p>
                 </div>
