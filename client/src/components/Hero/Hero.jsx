a6591d8e81ce7cff462e3cefbdd548c5b62f63f7
Ivelin Shenkov <ishenkito@gmail.com>
2024-09-04 23:02:37 +0300

    Update CV link

--- client/src/components/Hero/Hero.jsx
+++ client/src/components/Hero/Hero.jsx
@@ -12,7 +12,7 @@ const Hero = () => {
                     </h2>
                     <div className="flex justify-start mt-8">
                         <a
-                            href="https://drive.google.com/uc?export=download&id=1tZ7miY3CWrZp0uHzzeegZ8DMxqlsRUUs"
+                            href="https://drive.google.com/uc?export=download&id=1qyN510cclPdKW8QPYddra_3MEMVcGYDS"
                             download="Ivelin_Shenkov_CV.pdf"
                             className="bg-gradient-to-r from-purple-400 to-blue-500 text-white px-4 py-2 rounded-full transform transition-transform duration-300 hover:scale-105 hover:from-green-400 hover:to-blue-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-400"
                         >
